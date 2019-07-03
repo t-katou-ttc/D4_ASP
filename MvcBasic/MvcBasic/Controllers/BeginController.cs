364426f48a99a25770a9430c97541e9055bf844c
katoutoshiro <t-katou@ttc-net.co.jp>
2019-07-03 18:46:29 +0900

    量が多すぎる気するけどD4＿ASP作成しました!!

---
+++ MvcBasic/MvcBasic/Controllers/BeginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBasic.Models;
+using System.Diagnostics;
+
+namespace MvcBasic.Controllers
+{
+    public class BeginController : Controller
+    {
+        // GET: Begin
+        public ActionResult Index()
+        {
+            return Content("こんにちは、世界！");
+        }
+        public ActionResult Show()
+        {
+            ViewBag.Message = ("こんにちは、世界！");
+            return View();
+        }
+
+        private MvcBasicContext db = new MvcBasicContext();
+        public ActionResult List()
+        {
+            return View(db.Members);
+        }
+        public BeginController()
+        {
+            db.Database.Log = sql =>
+            {
+                Debug.Write(sql);
+            };
+        }
+    }
+}
---
+++ MvcBasic/MvcBasic/Controllers/MembersController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcBasic.Models;
+
+namespace MvcBasic.Controllers
+{
+    public class MembersController : Controller
+    {
+        private MvcBasicContext db = new MvcBasicContext();
+
+        // GET: Members
+        public ActionResult Index()
+        {
+            return View(db.Members.ToList());
+        }
+
+        // GET: Members/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        // GET: Members/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Members/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,Birth,Married,Memo")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Members.Add(member);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(member);
+        }
+
+        // GET: Members/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        // POST: Members/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Email,Birth,Married,Memo")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(member).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(member);
+        }
+
+        // GET: Members/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        // POST: Members/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            // Member member = db.Members.Find(id);
+            // db.Members.Remove(member);
+            // 下の2行か上の2行が必要
+            var m = new Member() { Id = id };
+            db.Entry(m).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();  // 接続を開放
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
